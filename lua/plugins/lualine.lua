87b2667bfb8faf7f917f3d9f4ff5b15d514f163e
ludka <ludka281@student.liu.se>
2026-05-20 13:42:09 +0200

    updated things

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,13 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  opts = {
-
-    options = {
-      -- component_separators = "",
-      section_separators = { left = "", right = "" },
-    },
-  },
-}
+if true then
+  return {}
+end
+-- return {
+--   "nvim-lualine/lualine.nvim",
+--   opts = {
+--
+--     options = {
+--       -- component_separators = "",
+--       section_separators = { left = "", right = "" },
+--     },
+--   },
+-- }
