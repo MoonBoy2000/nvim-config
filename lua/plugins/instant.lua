0665de13160a3842638db1c90f81043274be2589
Ludvig <ludka281@student.liu.se>
2026-05-26 11:07:52 +0200

    Reset of config

--- lua/plugins/instant.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "jbyuki/instant.nvim",
-  lazy = true, -- gör så att pluginet laddas direkt
-  config = function()
-    vim.g.instant_username = "Ludvig" -- byt till ditt namn eller alias
-  end,
-}
