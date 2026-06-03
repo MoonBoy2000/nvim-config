02cc3cc044837a4de4322e8269e048f894e24faa
Ludvig <ludka281@student.liu.se>
2026-06-03 10:21:54 +0200

    added somethings to instant

--- lua/plugins/instant.lua
+++ lua/plugins/instant.lua
@@ -1,6 +1,7 @@
 return {
   "jbyuki/instant.nvim",
   lazy = false, -- gör så att pluginet laddas direkt
+  cmd = { "InstantStartServer", "InstantJoinSession", "InstantStop" },
   config = function()
     vim.g.instant_username = "Ludvig" -- byt till ditt namn eller alias
   end,
